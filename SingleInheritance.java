559a1c5420ee3c5a12ad8000668e69374359aca5
Kritika Tiwari <119162765+KritikaTiwari138@users.noreply.github.com>
2023-10-14 15:06:06 +0530

    Create SingleInheritance.java

---
+++ SingleInheritance.java
@@ -0,0 +1,29 @@
+// Java program to illustrate the
+// concept of single inheritance
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+
+// Parent class
+class one {
+	public void print_geek()
+	{
+		System.out.println("Geeks");
+	}
+}
+
+class two extends one {
+	public void print_for() { System.out.println("for"); }
+}
+
+// Driver class
+public class Main {
+	// Main function
+	public static void main(String[] args)
+	{
+		two g = new two();
+		g.print_geek();
+		g.print_for();
+		g.print_geek();
+	}
+}
